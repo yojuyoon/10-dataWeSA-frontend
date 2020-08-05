edd1c55838807ef8b7d709263f81b5cbe4cb5cb3
h-sick <53730691+h-sick@users.noreply.github.com>
2020-08-05 15:21:42 +0900

    Add: 테이블 레이아웃 완성 및 mock data 테스트 및 리뷰사항 반영

--- src/Pages/CoronaVirus.js
+++ src/Pages/CoronaVirus.js
@@ -1,8 +1,42 @@
-import React from "react";
-import CoronaMain from "../Components/CoronaMain";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DataTable from "../Components/DataTable";
+import { dataTableAPI } from "../config";
 
-const CoronaVirus = () => {
-  return <CoronaMain />;
-};
+function CoronaVirus() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  let [selectedState, setSelectedState] = useState([]);
+
+  useEffect(() => {
+    const getApi = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        setData(null);
+        const response = await axios.get(`${dataTableAPI}`);
+        setData(response.data.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+
+    getApi();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error!</div>;
+  if (!data) return null;
+
+  return (
+    <>
+      {data.length && (
+        <DataTable selectedState={selectedState} setSelectedState={setSelectedState} data={data} />
+      )}
+    </>
+  );
+}
 
 export default CoronaVirus;
