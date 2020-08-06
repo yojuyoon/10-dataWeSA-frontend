ba08fc770459a53c5d5039d08e0dc1be7f486395
h-sick <53730691+h-sick@users.noreply.github.com>
2020-08-06 10:30:14 +0900

    Refactor: 리뷰 사항 반영 및 conflict 해결

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import Routes from "./Routes";
+import theme from "./Styles/Theme";
 import GlobalStyles from "./Styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import theme from "./Styles/Theme";
