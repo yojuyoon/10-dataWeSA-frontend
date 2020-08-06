3e1542c92d7c8135695ab986347ec4ee30eca782
h-sick <53730691+h-sick@users.noreply.github.com>
2020-08-06 10:30:14 +0900

    Refactor: 리뷰 사항 반영 및 conflict 해결

--- src/Pages/CoronaVirus.js
+++ src/Pages/CoronaVirus.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DataTable from "../Components/DataTable";
-import { dataTableAPI } from "../config";
+import Daily from "../Components/Daily";
+import { tableDataAPI } from "../config";
 
 function CoronaVirus() {
   const [loading, setLoading] = useState(false);
@@ -9,35 +10,35 @@ function CoronaVirus() {
   const [data, setData] = useState(null);
   let [selectedState, setSelectedState] = useState([]);
 
-  useEffect(() => {
-    const getApi = async () => {
-      try {
-        setLoading(true);
-        setError(null);
-        setData(null);
-        const response = await axios.get(`${dataTableAPI}`);
-        setData(response.data.data);
-      } catch (e) {
-        setError(e);
-      }
-      setLoading(false);
-    };
+  // useEffect(() => {
+  //   const getApi = async () => {
+  //     try {
+  //       setLoading(true);
+  //       setError(null);
+  //       setData(null);
+  //       const response = await axios.get(`${tableDataAPI}`);
+  //       setData(response.data.data);
+  //     } catch (e) {
+  //       setError(e);
+  //     }
+  //     setLoading(false);
+  //   };
 
-    getApi();
-  }, []);
+  //   getApi();
+  // }, []);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error!</div>;
-  if (!data) return null;
+  // if (loading) return <div>Loading...</div>;
+  // if (error) return <div>Error!</div>;
+  // if (!data) return null;
 
   return (
     <>
-      {data.length && (
+      {/* {data.length && (
         <DataTable selectedState={selectedState} setSelectedState={setSelectedState} data={data} />
-      )}
-      <LinePlotComponent />
+      )} */}
+      <Daily />
     </>
-    )
+  );
 }
 
 export default CoronaVirus;
