7e887dde9a0ec49716212bfaf78b30910fcb9e2e
James Seung Hyun Soun <jamessoun93@gmail.com>
2020-08-07 13:41:57 +0900

    Revert "[김효식] Refactor: 리뷰 사항 반영 및 conflict 해결" (#16)
    * Revert "Refactor: 리뷰 사항 반영 및 conflict 해결"
    
    This reverts commit 3e1542c92d7c8135695ab986347ec4ee30eca782.
    
    * Revert "git rebasing"
    
    This reverts commit 02b4896a65dbc5ec20e4aed90c01fa1b34ef0298.

---
+++ src/Components/LinePlot/MobilityChartOptions.js
@@ -0,0 +1,65 @@
+export const options = {
+  chart: {
+    type: "spline",
+    width: 1300,
+    height: 450,
+    backgroundColor: "#F6F6F6",
+  },
+
+  title: {
+    text: "",
+  },
+
+  yAxis: {
+    title: "",
+  },
+
+  legend: {
+    enabled: false,
+  },
+
+  tooltip: {
+    formatter: function () {
+      return "" + this.series.name + ": " + this.y + "%";
+    },
+  },
+
+  plotOptions: {
+    series: {
+      marker: {
+        enabled: false,
+        states: {
+          hover: {
+            enabled: false,
+          },
+        },
+      },
+      dataLabels: {
+        enabled: true,
+        crop: false,
+        overflow: "none",
+        align: "left",
+        verticalAlign: "middle",
+        formatter: function () {
+          var last = this.series.data[this.series.data.length - 1];
+          if (this.point.category === last.category) {
+            return (
+              '<span style="color:' +
+              this.series.color +
+              '">' +
+              this.series.name +
+              "</span>"
+            );
+          }
+          return "";
+        },
+      },
+    },
+  },
+
+  credits: {
+    enabled: false,
+  },
+
+  series: [],
+};
--- src/Pages/CoronaVirus.js
+++ src/Pages/CoronaVirus.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DataTable from "../Components/DataTable";
-import Daily from "../Components/Daily";
-import { tableDataAPI } from "../config";
+import { dataTableAPI } from "../config";
 
 function CoronaVirus() {
   const [loading, setLoading] = useState(false);
@@ -10,35 +9,35 @@ function CoronaVirus() {
   const [data, setData] = useState(null);
   let [selectedState, setSelectedState] = useState([]);
 
-  // useEffect(() => {
-  //   const getApi = async () => {
-  //     try {
-  //       setLoading(true);
-  //       setError(null);
-  //       setData(null);
-  //       const response = await axios.get(`${tableDataAPI}`);
-  //       setData(response.data.data);
-  //     } catch (e) {
-  //       setError(e);
-  //     }
-  //     setLoading(false);
-  //   };
+  useEffect(() => {
+    const getApi = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        setData(null);
+        const response = await axios.get(`${dataTableAPI}`);
+        setData(response.data.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
 
-  //   getApi();
-  // }, []);
+    getApi();
+  }, []);
 
-  // if (loading) return <div>Loading...</div>;
-  // if (error) return <div>Error!</div>;
-  // if (!data) return null;
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error!</div>;
+  if (!data) return null;
 
   return (
     <>
-      {/* {data.length && (
+      {data.length && (
         <DataTable selectedState={selectedState} setSelectedState={setSelectedState} data={data} />
-      )} */}
-      <Daily />
+      )}
+      <LinePlotComponent />
     </>
-  );
+    )
 }
 
 export default CoronaVirus;
