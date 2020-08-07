7d4219bd6b64eb054546ad61466bd5034ffc08aa
James Seung Hyun Soun <jamessoun93@gmail.com>
2020-08-07 13:41:20 +0900

    Revert "Refactor: 리뷰 사항 반영 및 conflict 해결"
    This reverts commit 3e1542c92d7c8135695ab986347ec4ee30eca782.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
 import Routes from "./Routes";
-import theme from "./Styles/Theme";
 import GlobalStyles from "./Styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import theme from "./Styles/Theme";
